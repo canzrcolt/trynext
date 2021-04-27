e8ed1448dd48ed58f127633a6df0534a15c73263
canzrcolt <canzrcolt@gmail.com>
2021-04-27 09:41:00 -0400

    switching computers but not deploying

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 export default function NotFound() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
   return (
     <>
       <div className="not-found">sorry bruv u on wrong page</div>
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,17 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return (
+  <>
+    <Head>
+      <title>Home</title>
+      <meta name="keywords" content="portfolio"></meta>
+    </Head>
+    return (
     <div className="content-textbox">
       <div className="content-section-1">
         <h1>Hey there, me llamo yeah</h1>
       </div>{" "}
     </div>
-  );
+    );
+  </>;
 }
